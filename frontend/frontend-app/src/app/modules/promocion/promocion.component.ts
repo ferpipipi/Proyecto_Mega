a5cd5bdcad33b97b7213279778448e5efa6f5179
MiguelAngelGRomero <miguel_143_gomez@hotmail.com>
2025-06-30 01:41:11 -0600

    Se agregaron rutas, se ignoro informacion sensible, se estructuro el proyecto y si

---
+++ frontend/frontend-app/src/app/modules/promocion/promocion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promocion',
+  imports: [],
+  templateUrl: './promocion.component.html',
+  styleUrl: './promocion.component.scss'
+})
+export class PromocionComponent {
+
+}
